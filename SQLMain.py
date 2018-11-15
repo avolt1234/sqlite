1a2312f35487a0deec03fb16d66a57441fb1b9b3
Alex Voultos <voultoa@gmail.com>
2018-11-15 13:18:06 -0500

    Initial Commit

--- SQLMain.py
+++ SQLMain.py
@@ -2,7 +2,6 @@ import sqlite3
 import os
 import image
 import base64
-import matplotlib.pyplot as plt
 
 def main():
 
@@ -14,13 +13,9 @@ def main():
                 IMAGE       BLOB            NOT NULL
                 );''')
 
-    print('Opened Database Successfully')
-    filePath = r'C:\Users\Alex PC\Desktop\Images\salmon.jpeg'
-    insertImage(filePath, conn)
-
-    extract_picture(conn, 'salmon.jpeg')
-
+    print 'Opened Database Successfully'
     return
+
 
 def insertImage(filePath):
     conn = sqlite3.connect('database.db')
@@ -28,11 +23,12 @@ def insertImage(filePath):
     fileName = os.path.basename(filePath)
 
     with open(filePath, 'rb') as input_file:
-        ablob = input_file.read()
+        ablob = base64.b64encode(input_file.read())
 
     conn.execute('INSERT INTO IMAGES (ID, IMAGE) values (?,?)',
                  (fileName, sqlite3.Binary(ablob)))
     conn.commit()
+
 
 def extract_picture(pictureID):
     conn = sqlite3.connect('database.db')
@@ -40,17 +36,22 @@ def extract_picture(pictureID):
     param = {'id': pictureID}
     fileName, blob = conn.execute(sql, param).fetchone()
 
-    print(blob)
-    return blob
+    blob = base64.b64decode(blob)
+
+    fh = open(r'C:\Users\Alex PC\Desktop\Images\test.jpeg', 'wb')
+    fh.write(blob)
+    fh.close()
+
+    print blob
 
 
 if __name__ == '__main__':
-    value = input('Enter 1 to initialize db, enter 2 to add image, enter 3 to get image: ')
+    value = raw_input('Enter 1 to initialize db, enter 2 to add image, enter 3 to get image: ')
     if value == '1':
         main()
     elif value == '2':
-        file = input('Enter the fileName: ')
+        file = raw_input('Enter the fileName: ')
         insertImage(file)
     else:
-        file = input('Enter Image Name: ')
+        file = raw_input('Enter Image Name: ')
         extract_picture(file)
